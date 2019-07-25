b0921debb62438db67d9ac7bbdafc4da83cb0fff
ozanerdogan90 <ozaerdogan@stu.okan.edu.tr>
2019-07-25 15:29:28 +0200

    files are added

---
+++ scripts/mongo_spin_up.ps1
@@ -0,0 +1,1 @@
+docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
---
+++ src/types/error/error-formatter.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from 'joi';
+
+export interface IFormattedError {
+    [key: string]: string[];
+}
+
+export function errorFormatter(error: ValidationError): object {
+    const formattedError: IFormattedError = {};
+
+    error.details.forEach((validationErrorItem) => {
+        const key = validationErrorItem.context && validationErrorItem.context.key || 'general';
+        const message = validationErrorItem.message.replace(/["]/ig, '');
+
+        formattedError[key] = formattedError[key] ? formattedError[key].concat(message) : [message];
+    });
+
+    return formattedError;
+}
